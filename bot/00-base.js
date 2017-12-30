a0f668dcb7b9e1c1bbb4888d33131861f0d4a139
Alessandro Segala <EgoAleSum@users.noreply.github.com>
2017-12-30 22:09:32 +0100

    Catching exceptions while replying

--- bot/00-base.js
+++ bot/00-base.js
@@ -2,22 +2,26 @@
 
 // Middleware for all messages
 module.exports = (state) => {
-    const {bot, telemetry} = state
+    const {bot, pino, telemetry} = state
 
-    bot.use((ctx, next) => {
-        if(!ctx ||
-            !ctx.from ||
-            !ctx.from.id ||
-            ctx.from.is_bot // Ignore messages coming from bots
-        ) {
-            // Do nothing
-            telemetry.info({action: 'ignored', from: ctx.from.id, text: (ctx.message) ? ctx.message.text : null})
-            return
-        }
-        else {
-            // Continue responding
-            telemetry.info({action: 'message', from: ctx.from.id, text: (ctx.message) ? ctx.message.text : null})
-            return next()
+    bot.use(async (ctx, next) => {
+        try {
+            if(!ctx ||
+                !ctx.from ||
+                !ctx.from.id ||
+                ctx.from.is_bot // Ignore messages coming from bots
+            ) {
+                // Do nothing beside logging
+                telemetry.info({action: 'ignored', from: ctx.from.id, text: (ctx.message) ? ctx.message.text : null})
+            }
+            else {
+                // Continue responding
+                telemetry.info({action: 'message', from: ctx.from.id, text: (ctx.message) ? ctx.message.text : null})
+                await next()
+            }
+        } catch(err) {
+            pino.fatal({type: 'exception', message: err.message, stack: err.stack})
+            ctx.replyWithMarkdown('**Ouch!** An internal error occurred and we cannot reply to you… Please try again!\nPS: one of our admin has been notified')
         }
     })
 }
